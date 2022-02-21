32bfe60e6e49026a76349a60721a534958993488
Samuel Noye <samuel.noye@amalitech.org>
2022-02-21 15:14:49 +0000

    Initial Commit

---
+++ Samnoye-iOSPortfolio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Samnoye-iOSPortfolio
+//
+//  Created by Samuel Noye on 21/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
